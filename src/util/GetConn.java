86c471873b5176f92b7d7f2da115133e1ab5bf69
HAHH9527 <1024830255@qq.com>
2017-05-14 22:08:42 +0800

    Conn[连接数据库]

---
+++ src/util/GetConn.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author 10248
+ */
+public class GetConn {
+
+    public static Connection GetConn() throws SQLException {
+        return DriverManager.getConnection("jdbc:sqlserver://locahost:1433;datebaseName=TX_MyJavaQQ_DB","sa","990401");
+    }
+    
+}

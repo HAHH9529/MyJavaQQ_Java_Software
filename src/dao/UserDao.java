08be5d826762f9e9df12a3171166b54794670ac2
HAHH9527 <1024830255@qq.com>
2017-05-16 16:28:21 +0800

    用户操作//登录验证

--- src/dao/UserDao.java
+++ src/dao/UserDao.java
@@ -17,17 +17,33 @@ import util.GetConn;
  * @author 10248
  */
 public class UserDao {
+
     public int userRegister(User user) throws SQLException {
         Connection conn = GetConn.GetConn();
-        PreparedStatement ps = conn.prepareStatement("use TX_MyJavaQQ_DB exec dbo.proc_CreateQQ ?,?,?,?,?,?");
+        PreparedStatement ps = conn.prepareStatement("use TX_MyJavaQQ_DB exec [dbo].[proc_CreateQQ] (?,?,?,?,?,?)");
         ps.setString(1, user.getUserNickname());
         ps.setString(2, user.getUserPassword());
         ps.setString(3, user.getUserSex());
         ps.setString(4, user.getUserBirthday());
         ps.setString(5, user.getUserAddress());
         ps.setString(6, user.getUserPhone());
-        ResultSet rs = ps.executeQuery();
+        ps.execute();
+        ResultSet rs = conn.prepareStatement("select top 1 User_Number from Table_User order by User_Number desc").executeQuery();
         int ret = rs.getInt("User_Number");
+        System.out.println(ret);
+        return ret;
+    }
+
+    public boolean userLogin(User user) throws SQLException {
+        boolean ret = false;
+        Connection conn = GetConn.GetConn();
+        PreparedStatement ps = conn.prepareStatement("select User_Password from View_Login where User_Number = ?");
+        ResultSet rs = ps.executeQuery();
+        String Password = rs.getString("User_Password");
+        String inputPassword = user.getUserPassword();
+        if (Password.equals(inputPassword)) {
+            ret = true;
+        }
         return ret;
     }
 }
